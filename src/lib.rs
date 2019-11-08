d219d06ec4f4078a4aa6e124782dd1a1883ab0ac
Joey <joey.xf@gmail.com>
2019-11-08 19:13:35 +0800

    基本的依赖注入

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,46 @@
-// #[macro_use]
-// extern crate lazy_static;
+#[macro_use]
+extern crate downcast_rs;
+use downcast_rs::DowncastSync;
+
 use std::collections::HashMap;
 
-// lazy_static! {
-//     static ref PRIVILEGES: HashMap<&'static str, Vec<&'static str>> = {
-//         let mut map = HashMap::new();
-//         map.insert("James", vec!["user", "admin"]);
-//         map.insert("Jim", vec!["user"]);
-//         map
-//     };
-// }
-// pub fn show_access(name: &str) -> &Vec<&'static str> {
-//     let access = PRIVILEGES.get(name).unwrap();
-//     return access;
-// }
+pub trait Resolvable {
+    fn resolve(&self, name: &str) -> &Box<dyn Injectable>;
+}
 
 pub struct Container {
     pub svcs: HashMap<&'static str, Box<dyn Injectable>>,
 }
 
+impl Resolvable for Container {
+    fn resolve(&self, name: &str) -> &Box<dyn Injectable> {
+        self.svcs.get(name).unwrap()
+    }
+}
+
 impl Container {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Container {
-            svcs:  HashMap::new(),
+            svcs: HashMap::new(),
         }
     }
 }
 
-pub trait Injectable {
+pub trait Injectable: DowncastSync {
     fn inject(self, c: &mut Container);
 }
+impl_downcast!(sync Injectable);
 
 pub struct Service1;
 impl Injectable for Service1 {
     fn inject(self, c: &mut Container) {
         c.svcs.insert("test1", Box::new(self));
+    }
+}
+
+impl Service1 {
+    pub fn hello(&self) {
+        println!("hello from service1");
     }
 }
 
@@ -49,5 +55,8 @@ mod tests {
         t1.inject(&mut c);
 
         assert!(c.svcs.contains_key("test1"));
+
+        let s1 = c.resolve("test1");
+        s1.downcast_ref::<Service1>().unwrap().hello();
     }
 }
